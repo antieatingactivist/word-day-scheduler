41bb28b74d22657cb759acb90415d1e11c17fb26
antieatingactivist <antieatingactivist@gmail.com>
2022-01-05 21:21:38 -0800

    created schedule block in js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,31 @@
 var dateEl = $('#currentDay');
+var timeBlocksEl = $('#time-blocks');
+var currentHour = 12;
 
 
-dateEl.text(moment().format('dddd, MMMM Do'));
+dateEl.text(moment().format('dddd, MMMM Do YYYY - h:mm:ss a'));
+timeBlocksEl.append($('<div class="container"></div>'));
+
+for (var i=9; i<=17; i++) {
+    var hour = i;
+    if (hour>12) hour-=12;
+    drawTimeBlock(i);
+    $('#'+i).children().first().children().text(hour+':00');
+    if (currentHour > i) {
+        $('#'+i).children().eq(1).addClass("past");
+    }
+    else if (currentHour === i) {
+        $('#'+i).children().eq(1).addClass("present");
+    }
+    else {
+        $('#'+i).children().eq(1).addClass("future");
+    }
+}
+
+
+
+function drawTimeBlock(id) {
+    timeBlocksEl.children().first().append($(
+        '<div class="input-group" id="'+ id +'"><div class="input-group-prepend"><span class="input-group-text">X:XX</span></div><textarea class="form-control" aria-label="With textarea"></textarea><div class="input-group-append"><span class="input-group-text saveBtn">🔓</span></div></div>'
+        ));
+}
